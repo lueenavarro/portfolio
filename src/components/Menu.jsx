47a5303ece40f818796ac47b2948acc9c09c1a8d
Louie <lnavarro6@dxc.com>
2020-12-03 14:03:32 +0800

    Add sidebar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { FiMenu } from "react-icons/fi";
 import "./Menu.scss";
 
 const Menu = () => {
+  const [showMenu, setShowMenu] = useState(false);
   const pages = [
     { label: "Home", link: "/" },
     { label: "Skills", link: "/skills" },
@@ -11,23 +13,33 @@ const Menu = () => {
     { label: "Contact", link: "/contact" },
   ];
 
+  const menuClass = "menu " + (showMenu ? "" : "menu--hidden");
+  
   return (
-    <nav className="menu">
-      <ul className="menu-list">
-        {pages.map((page, index) => (
-          <li key={index} className="menu-item">
-            <NavLink
-              to={page.link}
-              exact={true}
-              className="menu-link"
-              activeClassName="menu-link--active"
-            >
-              {page.label}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <>
+      <div className="menu-hamburger" onClick={() => setShowMenu(true)}>
+        <FiMenu scale={10} />
+      </div>
+      {showMenu ? <div className="menu-overlay" onClick={() => setShowMenu(false)}></div> : <></> }
+          <nav className={menuClass}>
+            <div className="menu-close" onClick={() => setShowMenu(false)}></div>
+            <ul className="menu-list">
+              {pages.map((page, index) => (
+                <li key={index} className="menu-item">
+                  <NavLink
+                    to={page.link}
+                    exact={true}
+                    className="menu-link"
+                    activeClassName="menu-link--active"
+                    onClick={() => setShowMenu(false)}
+                  >
+                    {page.label}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+    </>
   );
 };
 
