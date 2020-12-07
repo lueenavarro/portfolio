9060db548a38649d586473482ab92cf30ecbe366
Louie <lnavarro6@dxc.com>
2020-12-07 22:35:28 +0800

    Redesign work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import "./Work.scss";
 
 const Work = ({ screenshot, title, description, link }) => {
-    const work = <div className="work">
-      <img className="work-screenshot" src={screenshot} alt="" />
-      <div className="work-overlay">
-        <h3>{title}</h3>
-        <span className="work-description">{description}</span>
+  const work = (
+    <div className="work">
+      <div className="work-screenshot">
+        <img src={screenshot} alt="" />
+        <div className="work-overlay">
+          <h3>{title}</h3>
+        </div>
       </div>
+      <p className="work-description">{description}</p>
     </div>
-    
-    if(link) {
-        return <a className="work-link" href={link} target="_blank" rel="noreferrer">{work}</a>
-    } else {
-        return work
-    }
+  );
+
+  if (link) {
+    return (
+      <a className="work-link" href={link} target="_blank" rel="noreferrer">
+        {work}
+      </a>
+    );
+  } else {
+    return work;
+  }
 };
 
 export default Work;
