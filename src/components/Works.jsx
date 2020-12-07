dbd166950927cb6401043a111fadceb0c35cce52
Louie <lnavarro6@dxc.com>
2020-12-07 15:34:41 +0800

    Spell out POC

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -9,13 +9,13 @@ const Works = () => {
         <Work
           screenshot="screenshots/precious-finds.jpg"
           title="Precious Finds"
-          description="E-commerce site POC"
+          description="E-commerce site proof of concept"
           link="https://preciousfinds.netlify.app"
         />
         <Work
           screenshot="screenshots/esjs-hardware.jpg"
           title="Edgar SJ Santos Hardware"
-          description="Sales and Item tracker"
+          description="Sales and item tracker"
         />
       </div>
     </div>
