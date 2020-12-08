af5aba2ada92323f3a97c3f40264160ac006b391
Louie <lnavarro6@dxc.com>
2020-12-08 15:07:11 +0800

    Clear all timeouts on unmount

--- components/Typewriter.jsx
+++ components/Typewriter.jsx
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from "react";
 import style from "./Typewriter.module.scss";
 
-const Typewriter = ({message, speed = 200, pause = 3000}) => {
+const Typewriter = ({ message, speed = 200, pause = 3000 }) => {
   const [messageHolder, setMessageHolder] = useState("");
   const [counter, setCounter] = useState(0);
   const [blink, setBlink] = useState(false);
 
   useEffect(() => {
-    let timeout = setTimeout(() => {
-      if (counter < message.length) {
-        setMessageHolder(messageHolder.concat(message[counter]));
-        setCounter(counter + 1);
-      } else {
-        setBlink(true);
-        setTimeout(() => {
-          setBlink(false);
-          setMessageHolder("");
-          setCounter(0);
-        }, pause);
-      }
-    }, speed);
-    return () => clearInterval(timeout);
+    const timeouts = [];
+    timeouts.push(
+      setTimeout(() => {
+        if (counter < message.length) {
+          setMessageHolder(messageHolder.concat(message[counter]));
+          setCounter(counter + 1);
+        } else {
+          setBlink(true);
+          timeouts.push(
+            setTimeout(() => {
+              setBlink(false);
+              setMessageHolder("");
+              setCounter(0);
+            }, pause)
+          );
+        }
+      }, speed)
+    );
+    return () =>
+      timeouts.forEach((timeout) => {
+        clearInterval(timeout);
+      });
   });
 
   return (
