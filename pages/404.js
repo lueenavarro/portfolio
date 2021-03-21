a3aa6b6dc08d152baca0eeef185ad2ef62057b19
lueenavarro <47080916+lueenavarro@users.noreply.github.com>
2021-03-21 13:52:08 +0800

    Add proper 404 page

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const PageNotFound = () => {
+  return (
+    <Layout>
+      <div className="wrapper">
+        Sorry, we couldn't find the page you were looking for
+      </div>
+    </Layout>
+  );
+};
+
+export default PageNotFound;
