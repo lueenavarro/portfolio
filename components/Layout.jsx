2e531c329fb183f1ee8f24e45f6f2920f3fa96b9
lueenavarro <47080916+lueenavarro@users.noreply.github.com>
2021-03-30 21:36:06 +0800

    Change title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ function Layout({ children }) {
   return (
     <div className={styles.app}>
       <Head>
-        <title>Louie Navarro - A Fullstack Developer</title>
+        <title>Louie Navarro - Full Stack Developer</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content="Louie Navarro, Full Stack Developer, Philippines" />
