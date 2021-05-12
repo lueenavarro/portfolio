17c33e5e42a97e2336c40242eb7d1789e0d99035
lueenavarro <47080916+lueenavarro@users.noreply.github.com>
2021-05-13 04:02:49 +0800

    Add links in homepage

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,27 +1,44 @@
 import React from "react";
 import styles from "./home.module.scss";
-import Typewriter from '../components/Typewriter';
-import Layout from '../components/Layout';
+import Typewriter from "../components/Typewriter";
+import Layout from "../components/Layout";
 
 const Home = () => {
+  const bioLink = (link, icon) => (
+    <a href={link} target="_blank" rel="noopener noreferrer">
+      <img src={icon} alt="Github" />
+    </a>
+  );
+
   return (
     <Layout>
-    <div className="wrapper">
-      <div className={styles.homeContainer}>
-        <div className={styles.bio}>
-          <Typewriter message="I'm Louie. I'm a developer." />
-          <p className={styles.bioText}>
-            I'm a full stack developer by morning and evening. Hire me and I will take
-            care of your app from development to deployment.
-          </p>
-        </div>
-        <div className={styles.photo}>
-            <div className={`${styles.photoBlindfold} ${styles.photoBlindfoldBlack}`}></div>
-            <div className={`${styles.photoBlindfold} ${styles.photoBlindfoldRed}`}></div>
-            <img className={`${styles.photoImg}`} src="profile.png" alt=""/>
+      <div className="wrapper">
+        <div className={styles.homeContainer}>
+          <div className={styles.bio}>
+            <Typewriter message="I'm Louie. I'm a developer." />
+            <p className={styles.bioText}>
+              I'm a full stack developer by morning and evening. Hire me and I
+              will take care of your app from development to deployment.
+            </p>
+            <div className={styles.bioLinks}>
+              {bioLink("https://github.com/lueenavarro", "/logos/github.svg")}
+              {bioLink(
+                "https://www.linkedin.com/in/lueenavarro/",
+                "/logos/linkedin.svg"
+              )}
+            </div>
+          </div>
+          <div className={styles.photo}>
+            <div
+              className={`${styles.photoBlindfold} ${styles.photoBlindfoldBlack}`}
+            ></div>
+            <div
+              className={`${styles.photoBlindfold} ${styles.photoBlindfoldRed}`}
+            ></div>
+            <img className={`${styles.photoImg}`} src="profile.png" alt="" />
+          </div>
         </div>
       </div>
-    </div>
     </Layout>
   );
 };
