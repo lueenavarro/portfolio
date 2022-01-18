ce5c511be2623c024cfe0bf4a8a754140caa9741
Louie <louie.navarro@outlook.com>
2022-01-18 12:43:18 +0800

    Change url

--- pages/works.jsx
+++ pages/works.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from "react";
-import Layout from "../components/Layout";
-import Loader from "../components/Loader";
-import Slide from "../components/Slide";
-import Work from "../components/Work";
+import React, { useState, useEffect } from 'react';
+import Layout from '../components/Layout';
+import Loader from '../components/Loader';
+import Slide from '../components/Slide';
+import Work from '../components/Work';
 
-import styles from "./works.module.scss";
+import styles from './works.module.scss';
 
 const WorksList = () => {
   const [imgsLoaded, setImgsLoaded] = useState(false);
   const [slideShowImages, setSlideShowImages] = useState([]);
   const [showSlide, setShowSlide] = useState(false);
   const images = [
-    "/screenshots/esjs-hardware.jpg",
-    "/screenshots/precious-finds.jpg",
-    "/screenshots/covid-ph-stats.jpg",
-    "/screenshots/gawain.jpg",
+    '/screenshots/esjs-hardware.jpg',
+    '/screenshots/precious-finds.jpg',
+    '/screenshots/covid-ph-stats.jpg',
+    '/screenshots/gawain.jpg',
   ];
 
   useEffect(() => {
@@ -29,7 +29,7 @@ const WorksList = () => {
 
     Promise.all(images.map((image) => loadImage(image)))
       .then(() => setImgsLoaded(true))
-      .catch((err) => console.log("Failed to load images", err));
+      .catch((err) => console.log('Failed to load images', err));
   });
 
   const handleOpenSlide = (images) => {
@@ -51,25 +51,25 @@ const WorksList = () => {
               <div className={styles.worksList}>
                 <Work
                   screenshot={images[0]}
-                  title="Edgar SJ Santos Hardware"
-                  description="Sales and item tracker"
+                  title='Edgar SJ Santos Hardware'
+                  description='Sales and item tracker'
                   onClick={() =>
                     handleOpenSlide([
-                      "esjs/1.jpg",
-                      "esjs/2.jpg",
-                      "esjs/3.jpg",
-                      "esjs/4.jpg",
-                      "esjs/5.jpg",
+                      'esjs/1.jpg',
+                      'esjs/2.jpg',
+                      'esjs/3.jpg',
+                      'esjs/4.jpg',
+                      'esjs/5.jpg',
                     ])
                   }
-                  role="Full Stack Developer"
+                  role='Full Stack Developer'
                 />
                 <Work
                   screenshot={images[1]}
-                  title="Precious Finds"
-                  description="E-commerce site proof of concept"
-                  link="https://preciousfinds.netlify.app"
-                  role="Front End Developer"
+                  title='Precious Finds'
+                  description='E-commerce site proof of concept'
+                  link='https://preciousfinds.netlify.app'
+                  role='Front End Developer'
                 />
               </div>
               <br />
@@ -77,23 +77,23 @@ const WorksList = () => {
               <div className={styles.worksList}>
                 <Work
                   screenshot={images[2]}
-                  title="Covid PH Tracker"
-                  description="Tracks covid cases per region. Data is webscraped daily from Wikipedia using AWS Lambda, S3 and API Gateway."
-                  link="https://covid-ph-stats.netlify.app"
+                  title='Covid PH Tracker'
+                  description='Tracks covid cases per region. Data is webscraped daily from Wikipedia using AWS Lambda, S3 and API Gateway.'
+                  link='https://covid-ph-stats.netlify.app'
                 />
                 <Work
                   screenshot={images[3]}
-                  title="Gawain"
-                  description="Another task management application"
-                  link="https://gawain.ml/"
+                  title='Gawain'
+                  description='Another task management application'
+                  link='https://gawain.louienavarro.me/'
                   repos={[
                     {
-                      title: "Client",
-                      link: "https://github.com/lueenavarro/gawain-client",
+                      title: 'Client',
+                      link: 'https://github.com/lueenavarro/gawain-client',
                     },
                     {
-                      title: "API",
-                      link: "https://github.com/lueenavarro/gawain-api",
+                      title: 'API',
+                      link: 'https://github.com/lueenavarro/gawain-api',
                     },
                   ]}
                 />
