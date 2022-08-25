49dca0b5e6612fc7b7a3bc54bb8e240e3bdc0274
Louie <louie.navarro@outlook.com>
2022-08-25 22:52:21 +0800

    Update skills page

--- pages/skills.jsx
+++ pages/skills.jsx
@@ -1,32 +1,33 @@
-import React from "react";
-import Layout from "../components/Layout";
-import Skill from "../components/Skill";
-import styles from "./skills.module.scss";
+import React from 'react';
+import Layout from '../components/Layout';
+import Skill from '../components/Skill';
+import styles from './skills.module.scss';
 
 const SkillsList = () => {
   return (
     <Layout>
       <div className={`${styles.skills} wrapper`}>
         <div className={styles.skillsContainer}>
-          <Skill skill="JavaScript" rating={5} />
-          <Skill skill="Angular" rating={5} />
-          <Skill skill="React" rating={4} />
-          <Skill skill="Node.js" rating={5} />
-          <Skill skill="Express" rating={5} />
-          <Skill skill="Rest API" rating={5} />
-          <Skill skill="Python" rating={4} />
-          <Skill skill="Flask" rating={3} />
-          <Skill skill="Java" rating={3} />
-          <Skill skill="SQL" rating={4} />
-          <Skill skill="NoSQL" rating={2} />
-          <Skill skill="Docker" rating={4} />
-          <Skill skill="Kubernetes" rating={4} />
-          <Skill skill="Git" rating={5} />
-          <Skill skill="Github Actions" rating={3} />
-          <Skill skill="Jenkins" rating={3} />
-          <Skill skill="Machine Learning" rating={2} />
-          <Skill skill="React Native" rating={1} />
-          <Skill skill="Flutter" rating={1} />
+          <Skill skill='JavaScript / Typescript' rating={5} />
+          <Skill skill='Angular' rating={5} />
+          <Skill skill='React' rating={4} />
+          <Skill skill='Node.js' rating={5} />
+          <Skill skill='Express' rating={5} />
+          <Skill skill='Rest API' rating={5} />
+          <Skill skill='AWS Services' rating={4} />
+          <Skill skill='Python' rating={4} />
+          <Skill skill='Flask' rating={3} />
+          <Skill skill='Java' rating={3} />
+          <Skill skill='SQL' rating={4} />
+          <Skill skill='NoSQL' rating={4} />
+          <Skill skill='Docker' rating={4} />
+          <Skill skill='Kubernetes' rating={4} />
+          <Skill skill='Git' rating={5} />
+          <Skill skill='Github Actions' rating={3} />
+          <Skill skill='Jenkins' rating={3} />
+          <Skill skill='Machine Learning' rating={2} />
+          <Skill skill='React Native' rating={1} />
+          <Skill skill='Flutter' rating={1} />
         </div>
       </div>
     </Layout>
